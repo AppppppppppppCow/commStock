0fea70e803fbaec98bff19f0ff15599295e239f6
이성우 <swmi0226@jj.ac.kr>
2025-02-17 23:38:51 +0900

    초기 세팅2

--- src/main/java/com/commuStock/commuStock/repository/UserRepository.java
+++ src/main/java/com/commuStock/commuStock/repository/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.commuStock.commuStock.entity.user.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface  UserRepository extends JpaRepository<User, Long>{
     
@@ -12,4 +13,8 @@ public interface  UserRepository extends JpaRepository<User, Long>{
 
     // pid로 사용자를 조회하는 메서드 (pid가 기본 키이므로 필요 없음, 기본적으로 제공됨)
     Optional<User> findById(Long pid);
+
+//    @Query("SELECT * FROM user WHERE name = ?")
+    User findByName(String name);
+
 }
